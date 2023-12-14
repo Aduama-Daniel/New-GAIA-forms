41b539bc9b8db2d8c39e455559cf58f0ff79a07a
Aduama-Daniel <82862806+Aduama-Daniel@users.noreply.github.com>
2023-12-14 15:24:17 +0000

    Update language.blade.php

--- resources/views/membership/language.blade.php
+++ resources/views/membership/language.blade.php
@@ -17,7 +17,7 @@
       <div class="form-container">
       <img src="{{secure_asset('/images/logo2.png')}}" id="lang-logo" alt="GAIA logo">
       <h2>Club Registration Form</h2>
-      <form id="step1Form" action="{{route('membership.store', ['step' => 2]) }}" method="post">
+      <form id="step1Form" action="{{ secure_url('/store/2') }}" method="post">
 @csrf <!-- Laravel CSRF token -->   
       <h3>Language Proficiency</h3>
          <h4>English Proficiency</h4>
@@ -59,7 +59,7 @@
          </div>
 
          <div class="nav">
-            <button type="button"><a href="{{route('membership.form')}}" class="page-link">Back</a></button>   
+            <button type="button"><a href="{{secure_url('/form')}}" class="page-link">Back</a></button>   
            
             <button type="submit">Next</button>
          </div>
